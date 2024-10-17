03c51bf3ef00fb0ac172c26fcee816424236b1fa
test <testlibrary>
2024-10-17 11:20:55 +0200

    Prima versione della libreria somma

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sum_lib',
+    version='0.1',
+    description='A simple library for doing sums.',
+    author='TEST',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[],
+)
